c8f10660b9a11f160d4f63687a2efd0096c82f95
Ryan Azur <kodegowd64@gmail.com>
2023-05-15 14:48:02 +0800

    01routers

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
-console.log(window.location.pathname);
+const global = {
+  currentPage: window.location.pathname,
+};
+
+//Highlight active link
+function highlightActiveLink() {
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach((link) => {
+    if (link.getAttribute('href') === global.currentPage) {
+      link.classList.add('active');
+    }
+  });
+}
+
+function init() {
+  switch (global.currentPage) {
+    case '/':
+      console.log('Home');
+      break;
+    case '/shows.html':
+      console.log('Shows');
+      break;
+    case '/movie-details.html':
+      console.log('Movie Details');
+      break;
+    case '/tv-details.html':
+      console.log('TV Details');
+      break;
+    case '/search.html':
+      console.log('Search');
+      break;
+  }
+
+  highlightActiveLink();
+}
+
+document.addEventListener('DOMContentLoaded', init);
