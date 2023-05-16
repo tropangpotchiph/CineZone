7a76eee222128316d7e78ef0207add1cc000ef5b
Ryan Azur <kodegowd64@gmail.com>
2023-05-16 10:29:20 +0800

    09display

--- js/script.js
+++ js/script.js
@@ -268,15 +268,64 @@ async function search() {
   global.search.term = urlParams.get('search-term');
 
   if (global.search.term !== '' && global.search.term !== null) {
-    const results = await searchAPIData();
-    console.log(results);
+    const { results, total_pages, page } = await searchAPIData();
+
+    if (results.length === 0) {
+      showAlert('No results found');
+      return;
+    }
+
+    displaySearchResults(results);
+
+    document.querySelector('#search-term').value = '';
   } else {
     showAlert('Please enter search term');
   }
 }
 
+//Displaying Search Results
+function displaySearchResults(results) {
+  results.forEach((result) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    
+    <a href="${global.search.type}-details.html?id=${result.id}">
+   ${
+     result.poster_path
+       ? `<img src="https://image.tmdb.org/t/p/w500${
+           result.poster_path
+         }" class="card-img-top" alt="${
+           global.search.type === 'movie' ? result.title : result.name
+         }"/>`
+       : `<img
+   src="./images/no-image.jpg"
+   class="card-img-top"
+   alt="${global.search.type === 'movie' ? result.title : result.name}"
+ />`
+   }
+
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${
+        global.search.type === 'movie' ? result.title : result.name
+      }</h5>
+      <p class="card-text">
+        <small class="text-muted">Release: ${
+          global.search.type === 'movie'
+            ? result.release_date
+            : result.first_air_date
+        }</small>
+      </p>
+    </div>
+  
+    `;
+    document.querySelector('#search-results').appendChild(div);
+  });
+}
+
 //Show Alert
-function showAlert(message, className) {
+function showAlert(message, className = 'error') {
   const alertEl = document.createElement('div');
   alertEl.classList.add('alert', className);
   alertEl.appendChild(document.createTextNode(message));
